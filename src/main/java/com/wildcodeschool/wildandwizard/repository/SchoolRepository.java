d2f25d6ccfb8e406dde7be442273a0e0a85f573b
Sandra Roques <roquesandra@hotmail.fr>
2019-11-04 21:47:20 +0100

    challenge done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -9,11 +9,25 @@ import java.util.List;
 public class SchoolRepository {
 
     private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
-    private final static String DB_PASSWORD = "Horcrux4life!";
+    private final static String DB_USER = "root";
+    private final static String DB_PASSWORD = "YourPassword";
 
     public void deleteById(Long id) {
-        // TODO: delete a school from the database
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM school WHERE id=?"
+            );
+            statement.setLong(1, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to delete data");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<School> findAll() {
